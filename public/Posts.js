b8d0e6c449da90382f9ebc30366ccb4b1fb67ebe
DT_SampathKumar <bsampath.bandaru@dispatchtrack.com>
2024-06-28 12:42:42 +0530

    createdpostclass

--- public/Posts.js
+++ public/Posts.js
@@ -22,3 +22,56 @@ async function fetchData() {
     console.error("Error :", error);
   }
 }
+
+const userIdToName = {
+  1: "Athota Srilatha",
+  2: "Bandaru Sampath Kumar",
+  3: "Gollakaram Ganga Bhavani",
+  4: "Gowtham Meka",
+  5: "Mayuraj Ranawat",
+  6: "Narendiran K",
+  7: "Pratham Madhunapanthula",
+  8: "Riya Reddy",
+  9: "Shaik Sameer",
+  10: "Suriyan K",
+  11: "Vaishnavi Panta",
+  12: "Yateesh Tangudu",
+};
+
+class Post {
+  constructor(userId, title, body) {
+    this.userId = userId;
+    this.title = title;
+    this.body = body;
+    this.name = userIdToName[userId] || "Unknown";
+  }
+}
+
+async function updateData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    console.log("response:", data);
+
+    const posts = data.map(
+      (post) => new Post(post.userId, post.title, post.body)
+    );
+    console.log("Posts:", posts);
+
+    return posts;
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
+  }
+}
+
+async function fun() {
+  try {
+    const posts = await updateData();
+    console.log("Fetched Posts:", posts);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+fun();
