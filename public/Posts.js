08e36856cd8e88352a439eb5873b59fe490d7972
DT_SampathKumar <bsampath.bandaru@dispatchtrack.com>
2024-06-28 12:20:15 +0530

    fetchedData

--- public/Posts.js
+++ public/Posts.js
@@ -4,17 +4,21 @@ async function fetchData() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data = await response.json();
-    const tBody = document.getElementById("tBody");
+
     console.log(data);
-    let tableData = "";
-    data.map((post) => {
-      tableData += `<tr>
-            <td>${post.id}</td>
-            <td>${post.title}</td>
-            <td>${post.body}</td>
-        </tr>`;
+    const tBody = document.getElementById("tBody");
+    tBody.innerHTML = "";
+    data.forEach((post) => {
+      const newRow = `
+                        <trow>
+                            <td>${post.id}</td>
+                            <td>${post.title}</td>
+                            <td>${post.body}</td>
+                        </trow>
+                    `;
+      tBody.innerHTML += newRow;
     });
   } catch (error) {
-    console.error("Error fetching posts:", error);
+    console.error("Error :", error);
   }
 }
