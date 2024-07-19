936f05d5e20e3a5ba4762a81f234b3f7132517a7
DT_SampathKumar <bsampath.bandaru@dispatchtrack.com>
2024-07-19 16:00:51 +0530

    added_controller_views_using_scaffold

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,9 @@
 class Player < ApplicationRecord
     belongs_to :team
-
+    
+    validates :name, presence: true
+    validates :age, presence: true
+    validates :position, presence: true
+    validates :team, presence: true
+    
 end
